22ecf5e0768d6b6a6041d5ceba34c2a714600fb5
Dan d'Auge <76149200+danhype@users.noreply.github.com>
2021-01-25 17:52:39 +0100

    French translation

---
+++ languages/fr.php
@@ -0,0 +1,10 @@
+<?php
+
+return array (
+  'thewire-websitecards:readmore' => 'Lire plus',
+  'thewire-websitecards:settings:top' => 'Voici quelques réglages pour les cartes de site à afficher dans le flux.',
+  'thewire-websitecards:settings:api' => 'Clé de l\'API OpenGraph.io',
+  'thewire-websitecards:settings:apimore' => 'Ceci est facultatif. Laissez vide pour utiliser l\'OpenGraph Finder intégré par <a href="https://github.com/scottmac/opengraph/" target="_BLANK">scottmac</a>. Ceci  peut entraîner un ralentissement du chargement des pages.',
+  'thewire-websitecards:settings:css' => 'Style CSS des cartes de site',
+  'thewire-websitecards:settings:cssmore' => 'Changez le nom de la classe si vous souhaitez modifier le style de la carte de site à afficher. Écrivez du CSS personnalisé pour les cartes de site dans votre propre plugin (ou dans custom css).',
+);
